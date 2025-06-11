9e58979e14b7abcef24ad3ef8cf8de6a5f19f72c
Mithun Sivanesan <mithunsivanesan@gmail.com>
2025-06-11 16:40:13 +1000

    Add timer controls and update layout for better usability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { TimerSelect } from "./timer_select";
 export default function Home() {
   const [mins, setMins] = useState(25);
   const [secs, setSecs] = useState(0);
+  const [isSelect, setIsSelect] = useState(true);
 
   function handleMins(e: React.ChangeEvent<HTMLInputElement>) {
     setMins(Number(e.target.value));
@@ -17,8 +18,12 @@ export default function Home() {
     setSecs(Number(e.target.value));
   }
 
+  function handleTimer(e: React.MouseEvent<HTMLButtonElement>) {
+    setIsSelect(!isSelect ? true : false);
+  }
+
   return (
-    <div className="font-mono flex flex-col items-center justify-center max-w-screen">
+    <div className="font-mono flex flex-col items-center justify-center max-w-screen gap-4">
       <h1 className="font-bold my-6 text-2xl w-auto">POMODORO TIMER</h1>
       <TimerSelect
         mins={mins}
@@ -26,6 +31,12 @@ export default function Home() {
         handleMins={handleMins}
         handleSecs={handleSecs}
       />
+      <button
+        className="bg-gray-500 rounded-3xl text-l p-2"
+        onClick={handleTimer}
+      >
+        {isSelect ? "Start" : "Stop"}
+      </button>
     </div>
   );
 }
--- src/app/timer_select.tsx
+++ src/app/timer_select.tsx
@@ -14,9 +14,9 @@ export function TimerSelect({
   handleMins,
   handleSecs,
 }: TimerSelectProps) {
-  function handleMinChange(e: React.MouseEvent<HTMLButtonElement>) {
-    const id = (e.currentTarget as HTMLButtonElement).id;
-
+  function handleMinChange(e: React.MouseEvent<HTMLElement>) {
+    const id = (e.currentTarget as HTMLElement).id;
+    console.log(id);
     if (id === "min_up") {
       const value = mins + 1 > 59 ? "0" : Math.min(mins + 1, 59).toString();
       handleMins({
@@ -25,79 +25,83 @@ export function TimerSelect({
     } else if (id === "min_down") {
       const value = mins - 1 < 0 ? "59" : Math.max(mins - 1, 0).toString();
       handleMins({
-        target: { value: Math.max(mins - 1, 0).toString() },
+        target: { value: value },
       } as React.ChangeEvent<HTMLInputElement>);
     }
   }
 
   function handleSecChange(e: React.MouseEvent<HTMLButtonElement>) {
-    const id = (e.currentTarget as HTMLButtonElement).id;
+    const id = (e.currentTarget as HTMLElement).id;
     if (id === "sec_up") {
+      const value = secs + 1 > 59 ? "0" : Math.min(mins + 1, 59).toString();
       handleSecs({
-        target: { value: Math.min(secs + 1, 59).toString() },
+        target: { value: value },
       } as React.ChangeEvent<HTMLInputElement>);
     } else if (id === "sec_down") {
+      const value = secs - 1 < 0 ? "59" : Math.max(secs - 1, 0).toString();
       handleSecs({
-        target: { value: Math.max(secs - 1, 0).toString() },
+        target: { value: value },
       } as React.ChangeEvent<HTMLInputElement>);
     }
   }
 
   return (
-    <div className="py-2 gap-3 flex flex-row items-center">
-      <div className="flex flex-col gap-6">
-        <button
-          id="min_up"
-          className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
-          onClick={handleMinChange}
-        >
-          <FaArrowUp />
-        </button>
-        <input
-          id="min"
-          type="text"
-          min={0}
-          max={59}
-          maxLength={2}
-          size={2}
-          value={mins < 10 ? `0${mins}` : mins}
-          onChange={handleMins}
-          className="bg-gray-500 rounded-3xl text-2xl text-center h-16"
-        ></input>
-        <button
-          id="min_down"
-          className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
-          onClick={handleMinChange}
-        >
-          <FaArrowDown />
-        </button>
-      </div>
-      <div className="flex flex-col gap-6">
-        <button
-          id="sec_up"
-          className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
-          onClick={handleSecChange}
-        >
-          <FaArrowUp />
-        </button>
-        <input
-          id="sec"
-          type="text"
-          min={0}
-          max={59}
-          maxLength={2}
-          size={2}
-          value={secs < 10 ? `0${secs}` : secs}
-          onChange={handleSecs}
-          className="bg-gray-500 rounded-3xl text-2xl text-center h-16"
-        />
-        <button
-          id="sec_down"
-          className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
-          onClick={handleSecChange}
-        >
-          <FaArrowDown />
-        </button>
+    <div className="flex flex-col w-auto h-auto">
+      <div className="py-2 gap-3 flex flex-row items-center">
+        <div className="flex flex-col gap-6">
+          <button
+            id="min_up"
+            className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
+            onClick={handleMinChange}
+          >
+            <FaArrowUp />
+          </button>
+          <input
+            id="min"
+            type="text"
+            min={0}
+            max={59}
+            maxLength={mins < 10 ? 3 : 2}
+            size={2}
+            value={mins < 10 ? `0${mins}` : mins}
+            onChange={handleMins}
+            className="bg-gray-500 rounded-2xl text-2xl text-center h-16 w-12"
+          ></input>
+          <button
+            id="min_down"
+            className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
+            onClick={handleMinChange}
+          >
+            <FaArrowDown />
+          </button>
+        </div>
+        <div className="flex flex-col gap-6">
+          <button
+            id="sec_up"
+            className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
+            onClick={handleSecChange}
+          >
+            <FaArrowUp />
+          </button>
+          <input
+            id="sec"
+            type="text"
+            min={0}
+            max={59}
+            maxLength={secs < 10 ? 3 : 2}
+            size={2}
+            value={secs < 10 ? `0${secs}` : secs}
+            onChange={handleSecs}
+            className="bg-gray-500 rounded-2xl text-2xl text-center h-16 w-12"
+          />
+          <button
+            id="sec_down"
+            className="flex flex-row bg-gray-500 size-8 justify-center align-middle self-center p-2 rounded-3xl"
+            onClick={handleSecChange}
+          >
+            <FaArrowDown />
+          </button>
+        </div>
       </div>
     </div>
   );
